e3a8afdbf609902a1473563a2bb057497a941260
JMRMEDEV <josue.martinezrz@udlap.mx>
2020-09-04 10:04:41 -0500

    Add GestureDetector

---
+++ Kinectronics/GestureDetector.cs
@@ -0,0 +1,36 @@
+﻿namespace Kinectronics
+{
+    using Microsoft.Kinect;
+    using System;
+    using Kinectronics.GestureDataBases;
+    using System.Windows.Controls;
+
+    public class GestureDetector
+    {
+
+        private TextBlock textblock_gd;
+        private KTDef1 gestureDB;
+
+        public GestureDetector(KinectSensor kinectSensor, TextBlock textblock)
+        {
+            if (kinectSensor == null)
+            {
+                throw new ArgumentNullException("kinectSensor");
+            }
+            gestureDB = new KTDef1();
+            textblock_gd = textblock;
+        }
+
+        public void detectGesture(Body body)
+        {
+            if (body != null)
+            {
+                if (body.IsTracked)
+                {
+                    this.textblock_gd.Text = gestureDB.getGesture(body);
+                }
+            }
+        }
+
+    }
+}
