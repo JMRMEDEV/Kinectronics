c4b3e543aab8035c4cbd641336d99f960cc9eb27
JMRMEDEV <josue.martinezrz@udlap.mx>
2020-09-18 03:10:06 -0500

    Add class for EV3 Arm.

---
+++ Kinectronics/Devices/Kinectronics/Arm.cs
@@ -0,0 +1,41 @@
+﻿namespace Kinectronics
+{
+    public class Arm : Device
+    {
+        public Arm(string connectionString) : base(connectionString)
+        {
+        }
+
+        public virtual void RotateBaseJoint()
+        {
+        }
+
+        public virtual void RotateShoulderJoint()
+        {
+        }
+
+        public virtual void RotateElbowJoint()
+        {
+        }
+
+        public virtual void RotateWristJoint1()
+        {
+        }
+
+        public virtual void RotateWristJoint2()
+        {
+        }
+
+        public virtual void RotateWristJoint3()
+        {
+        }
+
+        public virtual void ActivateTool()
+        {
+        }
+
+        public virtual void DeactivateTool()
+        {
+        }
+    }
+}
